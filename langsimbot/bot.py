d243ddbf46c644cdea955161ac4f0a1a59896fd2
samwaseda <o.waseda@mpie.de>
2024-07-21 19:41:12 +0000

    remove unused lines for now

--- langsimbot/bot.py
+++ langsimbot/bot.py
@@ -3,7 +3,6 @@ import telebot
 from llm import get_executor
 from collections import defaultdict
 import os
-from hashlib import SHA256
 
 
 messages = defaultdict(list)
@@ -35,8 +34,6 @@ def clear_message(message):
 
 @bot.message_handler(func=lambda message: True)
 def message_handler(message):
-    if not check_id(message.chat.id):
-        return
     messages[message.chat.id].append(("human", message.text))
     print(message.text)
     response = get_output(messages[message.chat.id])
